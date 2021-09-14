08ec499291de4754a4ec9ed7e7dbd24c6b133481
Oliver Foster <oliver.foster@kineo.com>
2021-09-14 12:07:00 +0100

    issue/134 Improved implementation

--- js/TrickleButtonView.js
+++ js/TrickleButtonView.js
@@ -30,7 +30,7 @@ class TrickleButtonView extends ComponentView {
 
   initialize() {
     this.openPopupCount = 0;
-    this.isAwaitingFinish = false;
+    this.isAwaitingPopupClose = false;
     this.wasButtonClicked = false;
     this.model.calculateButtonText();
     this.calculateButtonState();
@@ -95,7 +95,7 @@ class TrickleButtonView extends ComponentView {
   async onPopupClosed() {
     this.openPopupCount--;
     if (this.openPopupCount) return;
-    if (this.isAwaitingFinish) {
+    if (this.isAwaitingPopupClose) {
       // Had completed with an open popup, perform final part of finishing
       return this.finish();
     }
@@ -168,7 +168,7 @@ class TrickleButtonView extends ComponentView {
     if (controller.isKilled) return;
     if (this.openPopupCount > 0) {
       // Has completed with an open popup, defer finish until popup closed
-      this.isAwaitingFinish = true;
+      this.isAwaitingPopupClose = true;
       return;
     }
     await this.finish();
