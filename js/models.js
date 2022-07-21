6e55edee0dc475f9f972bd45e9ae2f9905af4281
Chris Steele <chris.steele@kineo.com>
2022-07-21 17:59:12 +0100

    Add guard to only unlock trickle-based models

--- js/models.js
+++ js/models.js
@@ -198,7 +198,9 @@ export function applyLocks() {
   const TrickleButtonModel = components.getModelClass('trickle-button');
   // Check all models for trickle potential
   Adapt.course.getAllDescendantModels(true).filter(model => model.get('_isAvailable')).forEach(siteModel => {
-    sideModel.set('_isLocked', false);
+    if (siteModel.has('_trickle') && siteModel.get('_trickle')._isEnabled) {
+      siteModel.set('_isLocked', false);
+    }
     const trickleConfig = getModelConfig(siteModel);
     if (!isEnabled(siteModel, { trickleConfig })) return;
     const isModelLocked = isLocked(siteModel, { trickleConfig });
